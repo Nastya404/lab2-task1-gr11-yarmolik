3378f30e93aabcfa72c4cc28fbac5ada99581ac9
Anastasiya <yormolik.anastasiya@gmail.com>
2026-02-25 16:52:22 +0000

    Add isPrime function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
-/* hello.c */
+/* main.c */
+/* Author: Yarmolik Anastasia, group 11 */
+/* Program to find all three-digit prime numbers(22 var) */
 #include <stdio.h>
 
-void main (void)
+/* Declaration of private functions */
+int isPrime(int num)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+	int i;
+
+	if(num < 2)
+		return 0;
+
+	for(i = 2; i * i <= num; ++i)
+	{
+		if(num % i == 0)
+			return 0;
+	}
+
+	return 1;
+}
+
+int main (void)
+{
+	printf("Results: \n");
+	for(int i = 101; i < 1000; ++i){
+		if(isPrime(i)){
+			printf ("%d ", i);	}
+	}
 }
